82804a026a9f8b456cd4a603bb8d3d9e994cb097
tassolutions25 <tsionmoges25@gmail.com>
2026-04-17 16:15:02 +0300

    updates

--- Project 2 EDA/eda.py
+++ Project 2 EDA/eda.py
@@ -8,12 +8,12 @@ df = pd.read_csv("cs-training.csv")
 
 df.drop(columns=["Unnamed: 0"], inplace=True, errors="ignore")
 
-# print(df.head())
+print(df.head())
 
 # Data Cleaning
-# print(
-#     "cleaning Null values", df.isnull().sum()
-# )  # -> Found many Null values for monthly income and number of dependents
+print(
+    "cleaning Null values", df.isnull().sum()
+)  # -> Found many Null values for monthly income and number of dependents
 
 # fill the monthly income with the median value because it isn't skewed by extreme millionaires and the number of dependents with the mode value (most likely 0) because it is count
 df["MonthlyIncome"] = df["MonthlyIncome"].fillna(df["MonthlyIncome"].median())
@@ -28,8 +28,8 @@ sns.countplot(
     x="SeriousDlqin2yrs", data=df
 )  # Shows count of unique values in a categorical variable
 plt.title("Distribution of Financial Distress (Target)")
-# plt.show()
-# print(df['SeriousDlqin2yrs'].value_counts(normalize=True))
+plt.show()
+print(df['SeriousDlqin2yrs'].value_counts(normalize=True))
 
 
 # now we use bivariate analysis to find correlation between the financial distress and the other columns and analyze the risk factor
@@ -39,13 +39,13 @@ plt.title("Distribution of Financial Distress (Target)")
 plt.figure(figsize=(10, 6))
 sns.boxplot(x="SeriousDlqin2yrs", y="age", data=df)
 plt.title("Age vs Financial Distress")
-# plt.show() # -> result -> Usually, younger borrowers( Age 35 to 50) are statistically higher risk than older, more established borrowers.
+plt.show() # -> result -> Usually, younger borrowers( Age 35 to 50) are statistically higher risk than older, more established borrowers.
 
 # 2. Considering past due behavior
 # Comparing NumberOfTimes90DaysLate to the Target
 sns.lmplot(x="NumberOfTimes90DaysLate", y="SeriousDlqin2yrs", data=df, logistic=True)
 plt.title("Impact of 90+ Days Late Payments on Risk")
-# plt.show() # -> result -> this is more likely the strongest predictor. -> If a borrower has been 90 days late before, the probability of them being in distress again is very high.
+plt.show() # -> result -> this is more likely the strongest predictor. -> If a borrower has been 90 days late before, the probability of them being in distress again is very high.
 
 # detecting outliers -> in RevolvingUtilizationOfUnsecuredLines and in DebtRatio
 # Check for extreme values in Utilization
@@ -62,7 +62,7 @@ print("Debt Ratio: ", df[df["DebtRatio"] > 1].shape)
 plt.figure(figsize=(12, 10))
 sns.heatmap(df.corr(), annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Correlation Matrix")
-# plt.show() # -> This shows which variables "move together."
+plt.show() # -> This shows which variables "move together."
 
 # Analysis
 # Key Risk Predictors -> NumberOfTimes90DaysLate and RevolvingUtilizationOfUnsecuredLines
